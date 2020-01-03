5d987dba43cad3828bd7bae6a8f06a026ef39532
Mitchell Haugen <haugenmitch@gmail.com>
2020-01-03 15:10:33 -0500

    day 13 part 2

--- day13/arcade_cabinet.py
+++ day13/arcade_cabinet.py
@@ -5,20 +5,25 @@ with open(sys.argv[1]) as f:
     program = [int(n) for n in f.read().strip().split(',')]
 
 c = Computer(program)
-c.run()
-out = c.output()
 
-board = {}
+score = -1
+while not c.halted():
+    c.run()
+    board = {}
+    out = c.output()
+    paddle = -1
+    ball = -1
+    while len(out):
+        x = out.pop(0)
+        y = out.pop(0)
+        t = out.pop(0)
+        if x == -1 and y == 0:
+            score = t
+        else:
+            if t == 3:
+                paddle = x
+            elif t == 4:
+                ball = x
+    c.input(-1 if ball < paddle else 1 if paddle < ball else 0)
 
-while len(out):
-    x = out.pop(0)
-    y = out.pop(0)
-    t = out.pop(0)
-    board[(x, y)] = t
-
-count = 0
-for key in board:
-    if board[key] == 2:
-        count += 1
-
-print(count)
+print(score)
