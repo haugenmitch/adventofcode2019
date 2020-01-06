3f732035608571250a92295a86cae24a042f16ab
Mitchell Haugen <haugenmitch@gmail.com>
2020-01-05 22:51:17 -0500

    day 15 part 2

--- day15/oxygen.py
+++ day15/oxygen.py
@@ -1,6 +1,5 @@
 import sys
 from computer import Computer
-import matplotlib.pyplot as plt
 from copy import deepcopy
 
 
@@ -26,71 +25,49 @@ def move(comp, position, step_count):
         if out == 0:
             continue
         else:
+            queue.append((new_comp, pos, step_count))
             if out == 2:
                 print(step_count)
-            queue.append((new_comp, pos, step_count))
+                return pos
+    return None
+
+
+def fill(position, step_count):
+    step_count += 1
+    n = (position[0], position[1] + 1)
+    s = (position[0], position[1] - 1)
+    w = (position[0] + 1, position[1])
+    e = (position[0] - 1, position[1])
+    nn = 0
+    sn = 0
+    wn = 0
+    en = 0
+    if n in layout and layout[n] != 0 and n not in flood:
+        flood[n] = step_count
+        nn = 1 + fill(n, step_count)
+    if s in layout and layout[s] != 0 and s not in flood:
+        flood[s] = step_count
+        sn = 1 + fill(s, step_count)
+    if w in layout and layout[w] != 0 and w not in flood:
+        flood[w] = step_count
+        wn = 1 + fill(w, step_count)
+    if e in layout and layout[e] != 0 and e not in flood:
+        flood[e] = step_count
+        en = 1 + fill(e, step_count)
+    return max(nn, sn, wn, en)
 
 
 c = Computer(sys.argv[1])
 layout = {(0, 0): 1}
 queue = [(c, [0, 0], 0)]
+oxygen_pos = []
 
 while len(queue):
     item = queue.pop(0)
-    move(item[0], item[1], item[2])
+    r = move(item[0], item[1], item[2])
+    if r != None:
+        oxygen_pos = r
 
-for key in layout:
-    x = key[0]
-    y = key[1]
-    t = layout[key]
-    k = 'k' if t == 0 else 'g' if t == 1 else 'y'
-    a = 0.25 if t == 1 else 1
-    plt.scatter(x, y, marker='s', color=k, alpha=a, s=100)
-plt.show()
+flood = {tuple(oxygen_pos): 0}
 
-# plt.ion()
-# plt.show()
-# while not c.halted():
-#     char = str(input('> '))
-#     new_pos = deepcopy(pos)
-#     if char == 'w':
-#         c.input(1)
-#         new_pos[1] += 1
-#     elif char == 's':
-#         c.input(2)
-#         new_pos[1] -= 1
-#     elif char == 'a':
-#         c.input(3)
-#         new_pos[0] -= 1
-#     elif char == 'd':
-#         c.input(4)
-#         new_pos[0] += 1
-#     else:
-#         print(char)
-#         continue
-#
-#     c.run()
-#     status = c.output().pop(0)
-#     if status == 0:
-#         layout[tuple(new_pos)] = 0
-#     elif status == 1:
-#         pos = deepcopy(new_pos)
-#         layout[tuple(pos)] = 1
-#     elif status == 2:
-#         pos = deepcopy(new_pos)
-#         layout[tuple(pos)] = 2
-#     else:
-#
-#         continue
-
-# plt.clf()
-# for key in layout:
-#     x = key[0]
-#     y = key[1]
-#     t = layout[key]
-#     k = 'k' if t == 0 else 'g' if t == 1 else 'y'
-#     a = 0.25 if t == 1 else 1
-#     plt.scatter(x, y, marker='s', color=k, alpha=a, s=100)
-#     # plt.pause(0.001)
-# plt.scatter(pos[0], pos[1], marker='o', color='r', s=100)
-# plt.pause(0.001)
+print(fill(tuple(oxygen_pos), 0))
