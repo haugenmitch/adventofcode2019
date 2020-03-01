dba01c43e142576cb721576556e3a508e1320040
Mitchell Haugen <haugenmitch@gmail.com>
2020-03-01 11:48:01 -0500

    day 22 part 2

--- day21/hopper.py
+++ day21/hopper.py
@@ -4,6 +4,6 @@ c = Computer('program.txt')
 c.run()
 print(''.join([chr(char) for char in c.output()]))
 
-c.input([ord(c) for c in 'NOT C J\nAND D J\nNOT A T\nOR T J\nWALK\n'])
+c.input([ord(c) for c in 'NOT A T\nNOT B J\nOR T J\nNOT C T\nOR T J\nAND D J\nNOT J T\nOR E T\nOR H T\nAND T J\nRUN\n'])
 c.run()
 print(''.join([chr(val) if val < 256 else str(val) for val in c.output()]))
