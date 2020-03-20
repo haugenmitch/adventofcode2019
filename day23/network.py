203517a6f50a7db64fdce08e710b24afb6ccd582
Mitchell Haugen <haugenmitch@gmail.com>
2020-03-19 23:21:14 -0400

    day 23 part 2

--- day23/network.py
+++ day23/network.py
@@ -8,11 +8,17 @@ for i in range(50):
 
 print('start')
 
+nat_x = -1
+nat_y = -1
+last_y = -1
 while True:
+    idle = True
     for i in range(50):
+        nics[i].run()
         if nics[i].input_requested():
             nics[i].input(-1)
         if nics[i].output_ready():
+            idle = False
             out = nics[i].output()
             print(out)
             while len(out):
@@ -20,9 +26,16 @@ while True:
                 x = out.pop(0)
                 y = out.pop(0)
                 if dest == 255:
-                    print(y)
-                    break
-                nics[dest].input([x, y])
-        nics[i].run()
+                    nat_x = x
+                    nat_y = y
+                else:
+                    nics[dest].input([x, y])
+
+    if idle:
+        if nat_y == last_y:
+            print(nat_y)
+            break
+        last_y = nat_y
+        nics[0].input([nat_x, nat_y])
 
 print('done')
